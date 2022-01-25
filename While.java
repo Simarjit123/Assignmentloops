b503fbb588692fd9d0c42bad357cb4a5f4ffbe34
Simarjit <simarjassal9@gmail.com>
2022-01-25 10:00:53 -0500

    example

---
+++ While.java
@@ -0,0 +1,14 @@
+
+public class While {
+
+	public static void main(String[] args) {
+		int counter = 1;
+		String name="Simarjit";
+
+		while (counter <= 5) {
+			System.out.println(name);
+			counter++;
+		}
+	}
+
+}
