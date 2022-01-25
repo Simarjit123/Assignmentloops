097fb1e0064fa003f7de3ffdc4996b83b4515116
Simarjit <simarjassal9@gmail.com>
2022-01-25 10:11:33 -0500

    example

---
+++ ForLoop.java
@@ -0,0 +1,10 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		String name = "simar";
+		for (int forcounter = 1; forcounter <= 1; forcounter++) {
+			System.out.println(name);
+		}
+	}
+}
