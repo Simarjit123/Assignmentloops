e8aa0b8cd671758b51c688d2985bdf11df06dc71
Simarjit <simarjassal9@gmail.com>
2022-01-25 10:09:28 -0500

    example

---
+++ DoWhile.java
@@ -0,0 +1,14 @@
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+
+		int counter = 1;
+		String paint="i am painting";
+		do {
+			System.out.println( paint);
+			counter++;
+		} while (counter <= 5);
+	}
+
+}
